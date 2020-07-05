413c360c268be7a5458875e9e6e38e991e5e40b2
ZenRay <jake2011ren@gmail.com>
2020-07-05 21:53:44 +0800

    feat($dataStructure): Add Stack version2
    使用 Node 方式创建 Stack

--- Optional/dataStructures/application/stack.py
+++ Optional/dataStructures/application/stack.py
@@ -2,7 +2,7 @@
 """
 使用 List 表达 Stack
 """
-
+import copy
 
 class Stack:
     def __init__(self, array_size=10):
@@ -49,3 +49,61 @@ class Stack:
     @is_empty.setter
     def is_empty(self, value):
         return NotImplemented
+
+
+"""
+第二个版本：
+第二个版本处理的方式，不是使用 List 来模拟，而是直接使用节点的方式。所以在处理上需要分别使用
+Node 和对应的数据来处理
+"""
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack2:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    
+    def push(self, value):
+        self.size += 1
+        if self.head is None:
+            self.head = Node(value)
+        else:
+            # 直接将新 Node 放在最前面，这样就是不用遍历数据，达到了 Stack 插入数据更快的效果
+            node = Node(value)
+            node.next = self.head
+            self.head = node
+        
+
+    def is_empty(self):
+        return self.size == 0
+
+    
+    def pop(self):
+        if self.is_empty():
+            return None
+            
+        value = self.head.value
+        self.size -= 1
+        self.head = self.head.next
+        return value
+
+    
+    def top(self):
+        return self.head.value
+
+    
+    @staticmethod
+    def reverse(stack):
+        stack = copy.deepcopy(stack)
+        result = Stack2()
+        while True:
+            value = stack.pop()
+            if value is None:
+                return result
+            else:
+                result.push(value)
